161ef8ca4a28967a999cb2d9f68a1cb98f4090b9
Astalan <Arsen_Vinogradoff@tut.by>
2015-02-08 18:23:56 +0400

    Обратная польская запись. Ещё не работает

--- DZ_S_dzC_017_1/code 2.c
+++ DZ_S_dzC_017_1/code 2.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- int main()
- {
-     int a=0, output=0;
-     char stack[10];
-     int top=-1;
-     scanf ("%d", a);
-     stack[++top]=a;
-     output=stack[--top];
-     printf ("%d", output);
-     return 0;
- }
+int main()
+{
+    int stack[1000];
+    char arr [1000];
+    for (int i=0; i<1000; i++)
+    {
+       stack[i]=0;
+    }
+    for (int i=0; i<1000; i++)
+    {
+       arr[i]=0;
+    }
+    int top=0, top2=0, input=0, output=0;
+    stack[-1]=top;
+    arr[-1]=top2;
+    printf ("\nEnter a formula in the format of RPN\n");
+    do{
+            scanf ("%d", &input);
+            switch (input)
+            {
+                case '%d': scanf ("%d", stack[top]); ++top; break;
+                case '%c': scanf ("%c", arr[top2]); ++top2; break;
+            }
+    }while (input!='\n');
+    while (stack[top]!=stack[0])
+    {
+        output=stack[top];
+        --top;
+        stack[top]=output+top;
+        printf ("%d", stack[top]);
+    }
+    return 0;
+}
