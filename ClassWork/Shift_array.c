74b591602b6d12e64f7cf86de9666def62de92ec
Astalan <Arsen_Vinogradoff@tut.by>
2015-02-18 00:43:44 +0400

    Yes

--- ClassWork/Shift_array.c
+++ ClassWork/Shift_array.c
@@ -5,57 +5,51 @@
 int main()
 {
     srand(time(NULL));
-    int myArray[2][6], number=0, i, j, c=0, s=0;
+    int myArray[2][6], number=0, i, j, c=0;
     for (i = 0; i < 2; ++i)
     {
         for (j = 0; j < 6; ++j)
         {
-            myArray[i][j] = rand()%6+1;
+            myArray[i][j] = rand()%12+1;
         }
     }
-    printf( "It`s first variant of our array: \n" );
+    printf( "It`s first variant of our array: \n\n\n" );
     for ( i = 0; i < 2; ++i)
     {
         for (j = 0; j < 6; ++j)
         {
-            printf( " %d ", myArray[i][j] );
+            printf( "%d\t", myArray[i][j] );
         }
         printf("\n");
     }
     printf ("\nPleace, get the number of shift: ");
     scanf ("%d", &number);
-    printf( "\nIt`s the second variant of the array: \n" );
-    for(i=1; i<=number; ++i)
+    printf( "\nIt`s the second variant of the array: \n\n\n" );
+    for(int a=1; a<=number; ++a)
     {
-        c=myArray[0][5];
-        myArray[0][5]=0;
-        myArray[0][5]=myArray[1][4];
-        myArray[0][4]=0;
-        myArray[0][4]=myArray[1][3];
-        myArray[0][3]=0;
-        myArray[0][3]=myArray[1][2];
-        myArray[0][2]=0;
-        myArray[0][2]=myArray[1][1];
-        myArray[0][1]=0;
-        myArray[0][1]=myArray[1][0];
-        myArray[0][0]=c;
-        myArray[1][5]=0;
-        myArray[1][5]=myArray[2][4];
-        myArray[1][4]=0;
-        myArray[1][4]=myArray[2][3];
-        myArray[1][3]=0;
-        myArray[1][3]=myArray[2][2];
-        myArray[1][2]=0;
-        myArray[1][2]=myArray[2][1];
-        myArray[1][1]=0;
-        myArray[1][1]=myArray[2][0];
-        myArray[1][0]=s;
+        int a=myArray[0][5], b=myArray[1][5];
+        for (i=0; i<2; ++i)
+        {
+            for (j=5; j>=0; --j)
+            {
+                myArray[i][j]=myArray[i][j-1];
+                if (j==0 && i==0)
+                {
+                    myArray[i][j]=a;
+                }
+                else
+                {
+                    if (j==0 && i ==1)
+                    myArray[i][j]=b;
+                }
+            }
+        }
     }
     for (i=0; i<2; ++i)
     {
         for (j=0; j<6; ++j)
         {
-            printf( " %d ", myArray[i][j] );
+            printf( "%d\t", myArray[i][j] );
         }
         printf("\n");
     }
