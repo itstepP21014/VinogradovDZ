ca1d6509e8e97ba4b9634574361e19026c8747fe
Astalan <Arsen_Vinogradoff@tut.by>
2015-04-20 00:53:08 +0400

    DZ

---
+++ DZ_S_dzC_028_1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n=0;
+    cout << "Enter the number: ";
+    cin >> n;
+    cout << "\n\n";
+    for (int i=0; i<n; ++i)
+    {
+        cout << i << "\n";
+    }
+    cout << n << "\n";
+    return 0;
+}
